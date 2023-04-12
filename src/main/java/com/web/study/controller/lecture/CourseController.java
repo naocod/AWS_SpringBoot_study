6b33a43ec38e43f8f1c9d5a571dc4839c8824ee9
naocod <8162857@naver.com>
2023-04-12 17:38:56 +0900

    aop로 예외 처리

--- src/main/java/com/web/study/controller/lecture/CourseController.java
+++ src/main/java/com/web/study/controller/lecture/CourseController.java
@@ -1,6 +1,12 @@
 package com.web.study.controller.lecture;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,9 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.web.study.aop.annotation.CheckNameAspect;
 import com.web.study.aop.annotation.ParamsAspect;
 import com.web.study.aop.annotation.TimerAspect;
+import com.web.study.aop.annotation.ValidAspect;
 import com.web.study.dto.DataResponseDto;
 import com.web.study.dto.ResponseDto;
 import com.web.study.dto.request.course.CourseReqDto;
+import com.web.study.dto.request.course.SearchCourseReqDto;
+import com.web.study.exception.CustomException;
 import com.web.study.service.CourseService;
 
 import lombok.RequiredArgsConstructor;
@@ -40,11 +49,30 @@ public class CourseController {
 		return ResponseEntity.ok().body(DataResponseDto.of(courseService.getCourseAll()));
 	}
 	
+	@ValidAspect
 	@ParamsAspect
 	@GetMapping("/search/courses") 							// @RequestParam 생략
-	public ResponseEntity<? extends ResponseDto> seachCourse(int type, String searchValue) {
-	
-		return ResponseEntity.ok().body(DataResponseDto.of(courseService.seachCourse(type, searchValue)));
+	public ResponseEntity<? extends ResponseDto> searchCourse(@Valid SearchCourseReqDto searchCourseReqDto, BindingResult bindingResult) {
+		
+//		if(bindingResult.hasErrors()) {
+//			Map<String, String> errorMap = new HashMap<>();
+//			
+//			bindingResult.getFieldErrors().forEach(error -> {
+//				errorMap.put(error.getField(), error.getDefaultMessage());
+//			});
+//			
+//			throw new CustomException("유효성 검사 실패", errorMap);
+//		}
+
+
+		return ResponseEntity.ok().body(DataResponseDto.of(courseService.seachCourse(searchCourseReqDto.getType(), searchCourseReqDto.getSearchValue())));
 	}
 
 }
+
+
+
+
+
+
+
